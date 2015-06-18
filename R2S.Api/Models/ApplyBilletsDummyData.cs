a5f9f9396b5a9fcce5a1eb30ebfa07088b05a715
Michael Seeberger <ctd.mh3@gmail.com>
2015-06-17 19:01:10 -0500

    Numerous WIP changes
    Many proto/WIP changes.  Refer to OpportunitiesController for main
    semi-stable code.

---
+++ R2S.Api/Models/ApplyBilletsDummyData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace R2S.Api.Models
+{
+
+
+    /// <summary>
+    /// Summary description for ApplyBilletsDummyData
+    /// </summary>
+    public class ApplyBilletsDummyData
+    {
+
+        public ApplyBillet[] ApplyBillets { get; set;}
+
+        public ApplyBilletsDummyData()
+	    {
+		    //
+		    // TODO: Add constructor logic here
+		    //
+
+            ApplyBillet[] tempApplyBillets = new ApplyBillet[3];
+            tempApplyBillets[0] = new ApplyBillet("Unit 1", "Billet 1");
+            tempApplyBillets[1] = new ApplyBillet("Unit 1", "Billet 2");
+            tempApplyBillets[2] = new ApplyBillet("Unit 2", "Billet 1");
+
+            ApplyBillets = tempApplyBillets;
+        }
+    }
+
+}
---
+++ R2S.Api/Models/OpportunityModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace R2S.Api.Models
+{
+    public class OpportunityModel
+    {
+        public string AdvertisementID { get; set; }
+        public string RankRange { get; set; } 
+        public string QualificationSummary { get; set; } 
+        public string MissionName { get; set; }
+        public DateTime ReportDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Location { get; set; }
+
+    }
+
+    /*
+    public class OpportunityDBContext : DbContext
+    {
+        public DbSet<Opportunity> Opportunities { get; set; }
+    }
+     * 
+     * */
+}
